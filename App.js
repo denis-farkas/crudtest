1854df05239e8fb2caa4f0334c7efa74ea9a58a2
denis farkas <dfarkas960@gmail.com>
2023-06-26 09:29:11 +0200

    initial

---
+++ App.js
@@ -0,0 +1,33 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+import Home from './views/home';
+
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+  return (
+    <>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen name="Home" component={Home}></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
